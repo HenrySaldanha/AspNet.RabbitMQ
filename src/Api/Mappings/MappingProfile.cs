36a51f8d492b026ec3da9e83549137e46743f557
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-03 19:54:51 -0300

    feat: add mapping configuration

---
+++ src/Api/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models.Request;
+using Api.Models.Response;
+using AutoMapper;
+using Domain;
+
+namespace Api.Mappings;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<UserRequest, User>();
+        CreateMap<User, UserResponse>();
+    }
+}
