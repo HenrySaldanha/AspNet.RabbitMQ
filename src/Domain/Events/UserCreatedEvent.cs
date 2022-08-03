16f8a192853b15899c553076ce63c5f45f843775
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-03 20:02:06 -0300

    feat: add domain entities

---
+++ src/Domain/Events/UserCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Events;
+
+public interface UserCreatedEvent
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ src/Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
