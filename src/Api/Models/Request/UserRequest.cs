29049e21b223f4bad13f97970a63c7fcadcbd72e
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-03 19:58:08 -0300

    feat: add request and response model

---
+++ src/Api/Models/Request/UserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Models.Request;
+
+public class UserRequest
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
