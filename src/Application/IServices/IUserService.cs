3e7de0536f264f563f1b2a1cad6feb48400c4d2d
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-03 20:03:37 -0300

    feat: add user service

---
+++ src/Application/IServices/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+namespace Application.IServices;
+
+public interface IUserService
+{
+    public Task<User> CreateAsync(User request);
+    public Task UpdateEmailAsync(Guid id, string email);
+    public User Get(Guid id);
+}
---
+++ src/Application/Services/UserService.cs
@@ -0,0 +1,59 @@
+﻿using Application.IServices;
+using Domain;
+using Domain.Events;
+using MassTransit;
+using Repository;
+
+namespace Application.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _repo;
+    private readonly IPublishEndpoint _publisher;
+
+    public UserService(IUserRepository repo, IPublishEndpoint publisher)
+    {
+        _repo = repo;
+        _publisher = publisher;
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        // TODO: Add log 
+        try
+        {
+            user.Id = Guid.NewGuid();
+            user.CreatedAt = DateTime.UtcNow;
+            await _repo.CreateUserAsync(user);
+
+            await _publisher.Publish<UserCreatedEvent>(new
+            {
+                user.Id,
+                user.Username,
+                user.PhoneNumber,
+                user.CreatedAt,
+                user.Email
+
+            });
+            return user;
+        }
+        catch (Exception e)
+        {
+            // TODO: Add log 
+            throw;
+        }
+    }
+
+    public Task UpdateEmailAsync(Guid id, string email)
+    {
+        // TODO: Add log 
+        // TODO: implement
+        throw new NotImplementedException();
+    }
+
+    public User Get(Guid id)
+    {
+        // TODO: Add log 
+        return _repo.Get(id);
+    }
+}
