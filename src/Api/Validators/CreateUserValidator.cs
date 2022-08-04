c29451e6d6898d6a737965f5befd72b72f79b285
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-03 22:32:09 -0300

    feat: add new validators

---
+++ src/Api/Validators/CreateUserValidator.cs
@@ -0,0 +1,13 @@
+﻿using Api.Models.Request;
+using FluentValidation;
+
+namespace Api.Validators;
+
+public class CreateUserValidator : AbstractValidator<CreateUserRequest>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(request => request.Email).NotNull().Matches(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+        RuleFor(request => request.PhoneNumber).NotNull().Matches(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$");
+    }
+}
---
+++ src/Api/Validators/UpdateEmailValidator.cs
@@ -0,0 +1,12 @@
+﻿using Api.Models.Request;
+using FluentValidation;
+
+namespace Api.Validators;
+
+public class UpdateEmailValidator : AbstractValidator<UpdateEmailRequest>
+{
+    public UpdateEmailValidator()
+    {
+        RuleFor(request => request.Email).NotNull().Matches(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+    }
+}
