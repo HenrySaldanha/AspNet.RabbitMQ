c45ed06c0086a82854a9b7ecd4cb1064d3dc41d9
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-08-03 22:34:02 -0300

    feat: add new methods in repository

--- src/Repository.SqlServer/UserRepository.cs
+++ src/Repository.SqlServer/UserRepository.cs
@@ -17,8 +17,27 @@ public class UserRepository : IUserRepository
         await _context.SaveChangesAsync();
     }
 
+    public async Task UpdateEmailAsync(Guid id, string email)
+    {
+        var user = _context.Users.FirstOrDefault(c => c.Id == id);
+        user.Email = email;
+        _context.Users.Update(user);
+
+        await _context.SaveChangesAsync();
+    }
+
     public User Get(Guid id)
     {
-        return _context.Users.FirstOrDefault(c=>c.Id == id);
+        return _context.Users.FirstOrDefault(c => c.Id == id);
+    }
+
+    public bool HasUserByEmail(string email)
+    {
+        return _context.Users.Any(c => c.Email == email);
+    }
+
+    public bool HasUserByUsername(string username)
+    {
+        return _context.Users.Any(c => c.Username == username);
     }
 }
--- src/Repository/IUserRepository.cs
+++ src/Repository/IUserRepository.cs
@@ -5,4 +5,7 @@ public interface IUserRepository
 {
     Task CreateUserAsync(User user);
     User Get(Guid id);
+    bool HasUserByEmail(string email);
+    bool HasUserByUsername(string username);
+    Task UpdateEmailAsync(Guid id, string email);
 }
